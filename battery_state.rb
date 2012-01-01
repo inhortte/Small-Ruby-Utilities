ba0c3c606a09773b369f3481cef31e73b39673f8
Larger One <inhortte@gmail.com>
2012-01-02 00:48:08 +0100

    rewrote for new arch kernel

--- battery_state.rb
+++ battery_state.rb
@@ -1,16 +1,18 @@
 #! /usr/bin/env ruby
 
-batt = 'BAT1'
-state = File.open("/proc/acpi/battery/#{batt}/state").read
-if state !~ /discharging/
-  puts 'Not Available'
-  exit
-end
-info = File.open("/proc/acpi/battery/#{batt}/info").read
+batt = '/sys/class/power_supply/BAT1'
+#state = File.open("/proc/acpi/battery/#{batt}/state").read
+#if state !~ /discharging/
+#  puts 'Not Available'
+#  exit
+#end
+#info = File.open("/proc/acpi/battery/#{batt}/info").read
 
-m = /remaining capacity:\s+(\d+)\smAh/.match(state)
-remaining = m[1]
-m = /last full capacity:\s+(\d+)\smAh/.match(info)
-last_full = m[1]
+#m = /remaining capacity:\s+(\d+)\smAh/.match(state)
+#remaining = m[1]
+#m = /last full capacity:\s+(\d+)\smAh/.match(info)
+#last_full = m[1]
 
+remaining = File.open("#{batt}/charge_now").read
+last_full = File.open("#{batt}/charge_full").read
 puts (remaining.to_f / last_full.to_f * 100.0).to_i.to_s + "%"
