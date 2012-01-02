1816cc0d018ee90452f294a78858253e34da860c
Larger One <inhortte@gmail.com>
2012-01-02 01:06:14 +0100

    rewrote for new arch kernel

--- battery_state.rb
+++ battery_state.rb
@@ -13,6 +13,7 @@ batt = '/sys/class/power_supply/BAT1'
 #m = /last full capacity:\s+(\d+)\smAh/.match(info)
 #last_full = m[1]
 
+status = File.open("#{batt}/status").read.chomp
 remaining = File.open("#{batt}/charge_now").read
 last_full = File.open("#{batt}/charge_full").read
-puts (remaining.to_f / last_full.to_f * 100.0).to_i.to_s + "%"
+puts status + " " + (remaining.to_f / last_full.to_f * 100.0).to_i.to_s + "%"
